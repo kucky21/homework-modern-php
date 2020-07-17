ccf049eea81c852e44376d2b78bbfaf3ee806513
kucky21 <luk.greg@seznam.cz>
2020-07-17 21:29:42 +0200

    oop

--- index.php
+++ index.php
@@ -1,42 +1,47 @@
 <?php
-//made by Lukáš Gregor 04.07.2020 version 1 
-$totals = array(
-    "error"=>0,
-    "warning"=>0,
-    "notice"=>0,
-    "info"=>0,
-    "emergency"=>0,
-    "alert"=>0,
-    "others"=>0,
-);
+//list for store messages
+$list=[ "error"=>0,
+        "warning"=>0,
+        "notice"=>0,
+        "info"=>0,
+        "emergency"=>0,
+        "alert"=>0,
+        "others"=>0,];
 
-$file=file_get_contents("example.log", TRUE);
-$rows= explode(PHP_EOL, $file);
-//foreach cycle to extract words from log
-foreach ($rows as $line) {
-    if(preg_match("/test\.(\w+)/", $line, $matches)){
-        $message=strtolower($matches[1]);
-       //checking conditions of input and writing data into "totals" array
-       if($message != "debug"){
-          if(array_key_exists($message, $totals)){
-              $totals[$message]++;
-          }else{
-              $totals["others"]++;
-          }
-       }
+class logSort{
+//made by Lukáš Gregor 04.07.2020 version 1 - procedural
+//made by Lukáš Gregor 17.07.2020 version 2 - OOP, type hint, iterables
+    public function saveLog(iterable $iterable){
+        $read=file_get_contents("example.log", TRUE);
+        $rows= explode(PHP_EOL, $read);
+            foreach($rows as $line){
+                if(preg_match("/test\.(\w+)/", $line, $matches)){
+                    $message=strtolower($matches[1]);
+                if($message != "debug"){
+                    if(array_key_exists($message, $iterable)){
+                        $iterable[$message]++;
+                    }else{
+                        $iterable["others"]++;
+                }
+            }
+        }
     }
+    return $iterable;        
 }
-//finall printing
-arsort($totals);
-foreach ($totals as $message => $number) {
-   echo "$message: $number" . PHP_EOL; 
-   echo "<br>";
+   
 }
+$a= new logSort();
+$b= $a->saveLog($list);
+
+
+//result print 
+foreach($b as $log => $count){
+        echo "$log: $count" . PHP_EOL; 
+        echo "<br>";
+}
+
+
 ?>
 
-<script>
-//reloading page using javascript (10sec)
-setTimeout(function(){
-    window.location.reload(1);
-}, 10000);
-</script>
+
+
