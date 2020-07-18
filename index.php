227dab34596be4917cfe6c71257b60253709210e
kucky21 <luk.greg@seznam.cz>
2020-07-18 11:11:08 +0200

    version 3

--- index.php
+++ index.php
@@ -6,12 +6,16 @@ class logSort{
 //made by Lukáš Gregor 15.07.2020 version 3 - anonymous function 
 
     public function saveLog(iterable $iterable){
+        //load log file
         $read=file_get_contents("example.log", TRUE);
         $rows= explode(PHP_EOL, $read);
+            //loop through the file
             foreach($rows as $line){
                 if(preg_match("/test\.(\w+)/", $line, $matches)){
                     $message=strtolower($matches[1]);
+                    //looking for exceptions
                 if($message != "debug"){
+                    //adding matches into the list
                     if(array_key_exists($message, $iterable)){
                         $iterable[$message]++;
                     }else{
@@ -23,6 +27,7 @@ class logSort{
     return $iterable;      
 }
     public function storeList(){
+        //list to store matches
         return $list=[ "error"=>0,"warning"=>0, "notice"=>0, "info"=>0, "emergency"=>0, "alert"=>0, "others"=>0,];
     }
 }
@@ -30,6 +35,7 @@ $a= $c= new logSort();
 $b= $a->storeList();
 $d= $c->saveLog($b);
 
+//anonymous function to print out results
 call_user_func(function() use ($d){
     foreach($d as $log => $count){
         echo "$log : $count". PHP_EOL;
